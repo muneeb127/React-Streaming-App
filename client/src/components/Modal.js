6c9f7a1280fc4e2a32ee2358bc286f931c22f6f5
Muneeb Ahmed Khan <muneebahmed_127@hotmail.com>
2019-12-09 20:27:49 +0500

    Complete the modal and add functionality to the DeleteStream Component. DeleteStream now wroking

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -11,9 +11,15 @@ const Modal = props => {
   //This function takes two arguments
   //We will create a new element in the index.html file and then render the protal into that element directly as a subelement of the body
   return ReactDOM.createPortal(
-    <div className="ui dimmer modals visible active">
-      <div className="ui standard modal visible active">
-        asdddddasdasd asdasd
+    <div onClick={props.onDismiss} className="ui dimmer modals visible active">
+      <div
+        onClick={e => e.stopPropagation()}
+        // e.stopPropagation() makes sure that event doesnot bubble up to the parent div and windows deosnot navigate to the other page
+        className="ui standard modal visible active"
+      >
+        <div className="header">{props.title}</div>
+        <div className="content">{props.content}</div>
+        <div className="actions">{props.actions}</div>
       </div>
     </div>,
     document.querySelector("#modal")
--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,13 +1,69 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import Modal from "../Modal";
+import history from "../../history";
+import { fetchStream, deleteStream } from "../../actions";
 
-const StreamDelete = () => {
-  return (
-    <div>
-      <h3>Stream Delete</h3>
-      <Modal />
-    </div>
-  );
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  renderActions() {
+    const { id } = this.props.match.params;
+
+    return (
+      <React.Fragment>
+        <button
+          onClick={() => this.props.deleteStream(id)}
+          className="ui negative button"
+        >
+          Delete
+        </button>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+      </React.Fragment>
+    );
+  }
+  // Wrap both buttons by a div because we cannot assign multiple values to a variable
+  //but this div will result in styling problems
+  // <div>
+  //   <button className="ui negative button">Delete</button>
+  //   <button className="ui button">Cancel</button>
+  // </div>
+
+  // So we wrap both button elements by ReactFragment
+  // ReactFragment is a jsx element that allows us to return multiple elements but doesnot produce any html (invisible element)
+
+  renderContent() {
+    if (!this.props.stream) {
+      return "Are you sure you want to delete this stream";
+    }
+    return `Are you sure you want to delete the stream with title: ${this.props.stream.title}`;
+  }
+
+  render() {
+    // console.log(this.props.match.params.id);
+    // console.log(this.props);
+    return (
+      <Modal
+        onDismiss={() => history.push("/")}
+        title="Delete Stream"
+        content={this.renderContent()}
+        actions={this.renderActions()}
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
 };
 
-export default StreamDelete;
+export default connect(mapStateToProps, { fetchStream, deleteStream })(
+  StreamDelete
+);
